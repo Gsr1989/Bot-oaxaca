28f208b463b34148ac8fd8bce2827498809b3176
Gsr1989 <empresalorens@gmail.com>
2025-08-23 16:46:23 -0600

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ from aiogram.types import FSInputFile
 from contextlib import asynccontextmanager, suppress
 import asyncio
 import qrcode
+from io import BytesIO  # ← ESTA LÍNEA TE FALTA
 
 # ------------ CONFIG ------------
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
