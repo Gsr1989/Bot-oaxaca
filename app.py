d15567251dd364a522af6b8e9dd20400589211dd
Gsr1989 <empresalorens@gmail.com>
2025-08-23 15:37:19 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, session, send_file
+from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
+from supabase import create_client, Client
+import fitz
+import os
+from fastapi import FastAPI, Request
+from aiogram import Bot, Dispatcher, types
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+from aiogram.filters import Command
+from aiogram.types import FSInputFile
+from contextlib import asynccontextmanager, suppress
+import asyncio
